a5b1e9102a0b7b850fad5a7c84685b12f055c125
afattahi1387 <afattahi1387@gmail.com>
2022-09-16 18:16:02 +0430

    added fa folder for persian messages.

---
+++ resources/lang/fa/validation.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'required' => 'فیلد :attribute الزامی است',
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    'attributes' => [
+        'password' => 'رمز عبور'
+    ],
+
+];
