088ad9c7de058d1415ddcd2148f57ac50f1fbef1
afattahi1387 <afattahi1387@gmail.com>
2022-09-16 18:20:21 +0430

    showing login link in header if user not logged in and showing logout link and dashboard link if user is logged in.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,9 +3,17 @@
         <a class="navbar-brand" href="{{ route('home') }}">{{ env('APP_NAME') }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            {{-- <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-            </ul> --}}
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                @if(Auth::check())
+                    <li class="nav-item"><a href="#" class="nav-link">داشبورد</a></li>
+                    <li class="nav-item"><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout_form').submit();" class="nav-link">خروج</a></li>
+                    <form action="{{ route('logout') }}" method="POST" id="logout_form">
+                        {{ csrf_field() }}
+                    </form>
+                @else
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">ورود</a></li>
+                @endif
+            </ul>
         </div>
     </div>
 </nav>
