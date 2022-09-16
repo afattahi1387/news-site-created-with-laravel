5738deb73becbc858bba651c556af086ab27bfd3
afattahi1387 <afattahi1387@gmail.com>
2022-09-16 19:01:35 +0430

    changed redirect url after login.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashborad';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
