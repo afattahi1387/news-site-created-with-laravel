c3c50ae2bc44bbc12641918f715cec11f3a96a41
afattahi1387 <afattahi1387@gmail.com>
2022-09-17 08:10:42 +0430

    added fillable array.

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['category_name'];
+
     public function news() {
         return $this->hasMany(News::class);
     }
