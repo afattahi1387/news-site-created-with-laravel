86b94d1def205f13344b2b699821103cc967407b
afattahi1387 <afattahi1387@gmail.com>
2022-09-18 15:04:51 +0430

    added fillable array to News model.

--- app/News.php
+++ app/News.php
@@ -9,6 +9,8 @@ class News extends Model
 {
     use SoftDeletes;
 
+    protected $fillable = ['name', 'image', 'category_id', 'short_description', 'long_description'];
+
     public function category() {
         return $this->belongsTo(Category::class);
     }
