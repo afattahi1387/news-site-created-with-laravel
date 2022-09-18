199bb8905eecd98961fde18adb07799c73e2b8a1
afattahi1387 <afattahi1387@gmail.com>
2022-09-19 00:34:00 +0430

    showing add news form validations.

--- resources/views/dashboard/add_news_form.blade.php
+++ resources/views/dashboard/add_news_form.blade.php
@@ -17,15 +17,35 @@
                     <div class="card-body" style="direction: rtl;">
                         <form action="{{ route('create.news') }}" method="POST">
                             {{ csrf_field() }}
-                            <input type="text" name="name" placeholder="نام خبر" class="form-control"><br>
+                            <input type="text" name="name" placeholder="نام خبر" value="{{ old('name') }}" class="form-control">
+                            @if($errors->has('name'))
+                                <span class="text-danger">{{ $errors->first('name') }}</span><br>
+                            @endif
+                            <br>
                             <select name="category_id" class="form-control">
                                 <option value="">دسته بندی مورد نظر خود را انتخاب کنید</option>
                                 @foreach($categories as $category)
-                                    <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                    @if(!empty(old('category_id')) && old('category_id') == $category->id)
+                                        <option value="{{ $category->id }}" selected>{{ $category->category_name }}</option>
+                                    @else
+                                        <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                    @endif
                                 @endforeach
-                            </select><br>
-                            <textarea name="short_description" id="short_description" rows="15" placeholder="معرفی خبر" class="form-control"></textarea><br>
-                            <textarea name="long_description" id="long_description" rows="15" placeholder="متن خبر" class="form-control"></textarea><br>
+                            </select>
+                            @if($errors->has('category_id'))
+                                <span class="text-danger">{{ $errors->first('category_id') }}</span><br>
+                            @endif
+                            <br>
+                            <textarea name="short_description" id="short_description" rows="15" placeholder="معرفی خبر" class="form-control">{{ old('short_description') }}</textarea>
+                            @if($errors->has('short_description'))
+                                <span class="text-danger">{{ $errors->first('short_description') }}</span><br>
+                            @endif
+                            <br>
+                            <textarea name="long_description" id="long_description" rows="15" placeholder="متن خبر" class="form-control">{{ old('long_description') }}</textarea>
+                            @if($errors->has('long_description'))
+                                <span class="text-danger">{{ $errors->first('long_description') }}</span><br>
+                            @endif
+                            <br>
                             <input type="submit" value="افزودن" class="btn btn-success">
                         </form>
                     </div>
