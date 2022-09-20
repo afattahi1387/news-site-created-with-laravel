8af5ced7088d8e611b5adafdfbfd54de5e786d33
afattahi1387 <afattahi1387@gmail.com>
2022-09-20 21:52:44 +0430

    edited contact us page title.

--- resources/views/main_views/contact-us.blade.php
+++ resources/views/main_views/contact-us.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>ارتباط با ما</title>
+    <title>تماس با ما</title>
     <!-- Favicon-->
     <link rel="icon" href="/images/icons/contact-us.png" />
     <!-- Core theme CSS (includes Bootstrap)-->
